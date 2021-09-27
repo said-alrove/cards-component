ab8a28b33a762621dc4f6f6400d551694e10a454
Said Rosas <saidmift@outlook.es>
2021-09-27 15:52:31 -0500

    Add the scripts and Json tasks to the Gulpfile file

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,3 +1,15 @@
+function scripts() {
+  const { src, dest } = require('gulp');
+  return src("./src/scripts/*.js")
+    .pipe(dest("./public/scripts"))
+}
+
+function json() {
+  const { src, dest } = require('gulp');
+  return src("./src/*.json")
+    .pipe(dest("./public"))
+}
+
 function dev() {
   // TASKS
   const 
@@ -22,14 +34,17 @@ function dev() {
   watch("./src/*.html", gulpConfig, html).on("change", reload);
   watch("./src/styles/*.pcss", gulpConfig, css).on("change", reload);
   watch("./src/assets/**/*", gulpConfig, images)
+  watch("./src/scripts/*.js", gulpConfig, scripts).on("change", reload);
+  watch("./src/*.json", gulpConfig, json).on("change", reload);
 }
 
 const 
- { series } = require('gulp'),
+ { series, parallel } = require('gulp'),
  { html } = require('./html.js'),
  { css } = require('./styles.js'),
  { images } = require('./images.js');
 // This is mandatory
-exports.build = series(html, css, images);
-// exports.default = dev;
-exports.default = css;
+exports.build = 
+  series(html, css, images, 
+  parallel(scripts, json));
+exports.default = dev;
